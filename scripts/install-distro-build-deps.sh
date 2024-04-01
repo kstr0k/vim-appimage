97125b06697a85236179a382a2aa3f5dda8ecf5a
Alin Mr <almr.oss@outlook.com>
2024-04-01 08:16:31 +0300

    kstr0k-build.yml: factor out apt install's

---
+++ scripts/install-distro-build-deps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -ue
+apt update && apt install -y \
+  autoconf \
+  lcov \
+  gettext \
+  libcanberra-dev \
+  libperl-dev \
+  python-dev \
+  python3.8-dev \
+  liblua5.3-dev \
+  lua5.3 \
+  ruby-dev \
+  tcl-dev \
+  cscope \
+  libgtk-3-dev \
+  desktop-file-utils \
+  libtool-bin \
+  at-spi2-core \
+  libsodium-dev
