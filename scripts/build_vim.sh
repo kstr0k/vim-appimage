6b406f4defaf7bf458e6fa50a9031da7d2b11918
Alin Mr <almr.oss@outlook.com>
2024-04-03 05:51:51 +0300

    build_vim.sh: reduce cd/pushd/popd's

--- scripts/build_vim.sh
+++ scripts/build_vim.sh
@@ -6,8 +6,8 @@
 set -e
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
-
 SRCDIR=$script_dir/../vim/src
+
 FEATURES=huge
 export CFLAGS="-Wno-deprecated-declarations"
 
@@ -24,29 +24,36 @@ CFG_OPTS+=( "--prefix=/usr" )
 NPROC=$(getconf _NPROCESSORS_ONLN)
 
 # Apply experimental patches
+apply_patches() (
 shopt -s nullglob
-pushd "${SRCDIR}"/..
+cd "${SRCDIR}"/..
 for i in ../patch/*.patch; do git apply -v "$i"; done
-popd
-shopt -u nullglob
+)
 
-cd "${SRCDIR}"
 
 # Build Vim - no X11
+build_vim() (
+cd "${SRCDIR}"
 rm -rf vim
 SHADOWDIR=vim make -e shadow
-pushd vim
+cd vim
 ADDITIONAL_ARG="--without-x --enable-gui=no --enable-fail-if-missing"
 ./configure --with-features=$FEATURES "${CFG_OPTS[@]}" $ADDITIONAL_ARG
 make -j$NPROC
-popd
+)
 
 # Build GVim
+build_gvim() (
+cd "${SRCDIR}"
 rm -rf gvim
 SHADOWDIR=gvim make -e shadow
-pushd gvim
+cd gvim
 ADDITIONAL_ARG="--enable-fail-if-missing"
 CFG_OPTS+=( "--enable-gui=gtk3" )
 ./configure --with-features=$FEATURES "${CFG_OPTS[@]}" $ADDITIONAL_ARG
 make -j$NPROC
-popd
+)
+
+apply_patches
+build_vim
+build_gvim
