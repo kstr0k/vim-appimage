d6b2251bb20490070de266146803e3a322f3ea28
Alin Mr <almr.oss@outlook.com>
2024-04-05 18:41:31 +0300

    appimage.sh: reorg, group building in a fun()

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -35,10 +35,10 @@ make_appimage()
 	   "$script_dir"/../assets/AppRun.extracted \
 	   "${APP}".AppDir/
 
-	export UPDATE_INFORMATION="gh-releases-zsync|vim|vim-appimage|latest|$APP-*x86_64.AppImage.zsync"
-	export OUTPUT="${APP}-${VERSION}.glibc${GLIBC}-${ARCH}.AppImage"
+	export LDAI_UPDATE_INFORMATION="gh-releases-zsync|vim|vim-appimage|latest|$APP-*x86_64.AppImage.zsync"
+	# ^ linuxdeploy's internal appimage plugin uses these
 
-	./linuxdeploy.appimage --appdir "$APP.AppDir" \
+	LDAI_OUTPUT="$APPIMG_FNAME" ./linuxdeploy.appimage --appdir "$APP.AppDir" \
 		-d "${SOURCE_DIR}/runtime/${LOWERAPP}.desktop" \
 		-i "${SOURCE_DIR}/runtime/${LOWERAPP}.png" \
 		${PLUGIN:-} \
@@ -47,56 +47,53 @@ make_appimage()
 
 github_actions_deploy()
 (
-	cd -P "${BUILD_BASE}"
-	if [ -n "$GITHUB_ACTIONS" ]; then
+		cd -P "${BUILD_BASE}"
 		# Copy artifacts to $GITHUB_WORKSPACE
-		TARGET_NAME=$(find "$PWD/" -type f -name "$APP-*.AppImage" -printf '%f\n')
-		printf '%s\n' "Copy $BUILD_BASE/$TARGET_NAME -> $GITHUB_WORKSPACE"
-		cp "$TARGET_NAME" "$GITHUB_WORKSPACE"
-		cp "$TARGET_NAME.zsync" "$GITHUB_WORKSPACE"
-
+		printf '%s\n' "Copy $BUILD_BASE/$APPIMG_FNAME -> $GITHUB_WORKSPACE"
+		cp "$APPIMG_FNAME" "$APPIMG_FNAME".zsync "$GITHUB_WORKSPACE"
+	
 		# Github Release Notes
 		(cd "$script_dir"; . release_notes.sh > "$GITHUB_WORKSPACE/release.body")
-	fi
 )
 
-script_dir=$(dirname "$(readlink -f "$0")")
-
-APP=${1:-GVim}
-
-if [ -n "$GITHUB_ACTIONS" ]; then
-    echo "GitHub Actions detected"
-    BUILD_BASE=$HOME
-else
-    BUILD_BASE="$script_dir/../build"
-    mkdir -p "$BUILD_BASE"
-fi
-
-pushd vim
-GIT_REV="$(git rev-parse --short HEAD)"
-# should use tag if available, else use 7-hexdigit hash
-VERSION="$(git describe --tags --abbrev=0 || git describe --always)"
-# SOURCE_DIR: /home/<user>/vim-appimage/vim
-SOURCE_DIR="$(git rev-parse --show-toplevel)"
-ARCH=$(arch)
-LOWERAPP=${APP,,}
-popd
-
+make_and_deploy() (
 # uses the shadowdir from build_vim.sh
-pushd vim/src/"$LOWERAPP"
+cd vim/src/"$LOWERAPP"
 
-GLIBC=$(find "${SOURCE_DIR}" -type f -executable -exec nm -j -D {} + 2>/dev/null | sed -ne '/@GLIBC_2[.]/{ s/.*@GLIBC_//; /^2[.][0-9][.]/d; /^2[.][0-9]$/d; p }' | uniq | sort --version-sort -r -u | head -n 1)
-#GLIBC=$(/lib/x86_64-linux-gnu/libc.so.6 | sed -ne 's/.*GLIBC \(2\.[0-9][0-9]*\).*/\1/p;q')  # system version might be higher than actually required
-
-# Prepare some source files
+# Prepare source files
 patch_desktop_files
-
 make install DESTDIR="${BUILD_BASE}/${APP}.AppDir" >/dev/null
 
 # Create Appimage
 make_appimage
 
 # Perform Github Deployment
-github_actions_deploy
+if [ -n "$GITHUB_ACTIONS" ]; then
+	github_actions_deploy
+fi
+)
 
-popd
+compute_glibc_version() {
+  find "${SOURCE_DIR}" -type f -executable -exec nm -j -D {} + 2>/dev/null | sed -ne '/@GLIBC_2[.]/{ s/.*@GLIBC_//; /^2[.][0-9][.]/d; /^2[.][0-9]$/d; p }' | uniq | sort --version-sort -r -u | head -n 1
+  #/lib/x86_64-linux-gnu/libc.so.6 | sed -ne 's/.*GLIBC \(2\.[0-9][0-9]*\).*/\1/p;q'  # system version might be higher than actually required
+}
+
+script_dir=$(dirname "$(readlink -f "$0")")
+
+[ "$#" != 0 ] || set -- GVim
+
+SOURCE_DIR=$script_dir/../vim
+BUILD_BASE=$script_dir/../build
+mkdir -p "$BUILD_BASE"
+: "${GLIBC:=$(compute_glibc_version)}"
+
+GIT_REV="$(git -C "${SOURCE_DIR}" rev-parse --short HEAD)"
+# should use tag if available, else use 7-hexdigit hash
+: "${VERSION:="$(git -C "${SOURCE_DIR}" describe --tags --abbrev=0 || git describe --always)"}"
+: "${APPIMG_FNAME_SFX:=${VERSION}.glibc${GLIBC}-$(arch).AppImage}"
+
+for APP; do
+  LOWERAPP=${APP,,}
+  APPIMG_FNAME=${APP}-${APPIMG_FNAME_SFX}
+  make_and_deploy
+done
